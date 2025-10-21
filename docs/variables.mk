e8b2ca275d52d2c263109a7c53995a3777f38f49
Jack Baldry <jack.baldry@grafana.com>
2025-10-21 14:01:42 +0100

    Add documentation infrastructure
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

---
+++ docs/variables.mk
@@ -0,0 +1,12 @@
+# List of projects to provide to the make-docs script.
+# Format is PROJECT[:[VERSION][:[REPOSITORY][:[DIRECTORY]]]]
+# The following PROJECTS value mounts content into the grafana-cloudwatch-datasource project, at the "latest" version, which is the default if not explicitly set.
+# This results in the content being served at /docs/grafana-cloudwatch-datasource/latest/.
+# The source of the content is the current repository which is determined by the name of the parent directory of the git root.
+# This overrides the default behavior of assuming the repository directory is the same as the project name.
+PROJECTS := grafana-cloudwatch-datasource::$(notdir $(basename $(shell git rev-parse --show-toplevel)))
+
+# Test the nightly image.
+export DOCS_IMAGE := grafana/docs-base:nightly
+
+export WEBSITE_MOUNTS := true
