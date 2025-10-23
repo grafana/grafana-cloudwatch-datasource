8bdd03eaf43a83584d05cc4872f52a2ef0cf622c
Ida Štambuk <ida.stambuk@grafana.com>
2025-10-23 16:45:27 +0200

    Logs: Don't add console link to every field in the logs response and prepare 12.2.0 (#238)

--- src/utils/datalinks.test.ts
+++ src/utils/datalinks.test.ts
@@ -1,4 +1,4 @@
-import { DataQueryRequest, DataQueryResponse, dateMath } from '@grafana/data';
+import { DataQueryRequest, DataQueryResponse, dateMath, FieldType } from '@grafana/data';
 import { DataSourceSrv, setDataSourceSrv } from '@grafana/runtime';
 
 import { CloudWatchQuery } from '../types';
@@ -18,10 +18,12 @@ describe('addDataLinksToLogsResponse', () => {
             {
               name: '@message',
               config: {},
+              values: ['log message one', 'log message two'],
             },
             {
               name: '@xrayTraceId',
               config: {},
+              values: ['id1', 'id2'],
             },
           ],
           refId: 'A',
@@ -64,14 +66,6 @@ describe('addDataLinksToLogsResponse', () => {
           fields: [
             {
               name: '@message',
-              config: {
-                links: [
-                  {
-                    url: "https://us-east-1.console.aws.amazon.com/cloudwatch/home?region=us-east-1#logs-insights:queryDetail=~(end~'2016-12-31T16*3a00*3a00.000Z~start~'2016-12-31T15*3a00*3a00.000Z~timeType~'ABSOLUTE~tz~'UTC~editorString~'stats*20count*28*40message*29*20by*20bin*281h*29~isLiveTail~false~source~(~'fake-log-group-one~'fake-log-group-two))",
-                    title: 'View in CloudWatch console',
-                  },
-                ],
-              },
             },
             {
               name: '@xrayTraceId',
@@ -89,6 +83,19 @@ describe('addDataLinksToLogsResponse', () => {
                 ],
               },
             },
+            {
+              name: '',
+              type: FieldType.string,
+              values: ['View this query in CloudWatch console', 'View this query in CloudWatch console'],
+              config: {
+                links: [
+                  {
+                    url: "https://us-east-1.console.aws.amazon.com/cloudwatch/home?region=us-east-1#logs-insights:queryDetail=~(end~'2016-12-31T16*3a00*3a00.000Z~start~'2016-12-31T15*3a00*3a00.000Z~timeType~'ABSOLUTE~tz~'UTC~editorString~'stats*20count*28*40message*29*20by*20bin*281h*29~isLiveTail~false~source~(~'fake-log-group-one~'fake-log-group-two))",
+                    title: 'View in CloudWatch console',
+                  },
+                ],
+              },
+            },
           ],
           refId: 'A',
         },
@@ -96,7 +103,7 @@ describe('addDataLinksToLogsResponse', () => {
     });
   });
 
-  it('should add data links to response from log groups, trimming :*', async () => {
+  it('should add a data link field to response from log groups, trimming :*', async () => {
     const mockResponse: DataQueryResponse = {
       data: [
         {
@@ -140,6 +147,11 @@ describe('addDataLinksToLogsResponse', () => {
           fields: [
             {
               name: '@message',
+            },
+            {
+              name: '',
+              type: FieldType.string,
+              values: [],
               config: {
                 links: [
                   {
@@ -197,6 +209,11 @@ describe('addDataLinksToLogsResponse', () => {
           fields: [
             {
               name: '@message',
+            },
+            {
+              name: '',
+              type: FieldType.string,
+              values: [],
               config: {
                 links: [
                   {
