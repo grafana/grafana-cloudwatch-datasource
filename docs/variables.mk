2a2a84775bc01417076fd889b7cbf7d59104d439
Jack Baldry <jack.baldry@grafana.com>
2025-11-10 10:34:10 +0000

    Mount at /docs/grafana/grafana-cloudwatch-datasource/latest/
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- docs/variables.mk
+++ docs/variables.mk
@@ -1,4 +1,3 @@
 # List of projects to provide to the make-docs script.
 # Format is PROJECT[:[VERSION][:[REPOSITORY][:[DIRECTORY]]]]
-PROJECTS := grafana::$(notdir $(basename $(shell git rev-parse --show-toplevel)../grafana)) \
-	arbitrary:$(shell git rev-parse --show-toplevel)/docs/sources:/hugo/content/docs/grafana/latest/datasources/aws-cloudwatch \
+PROJECTS := plugins/grafana-cloudwatch-datasource
